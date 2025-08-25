982faa7d2c3b5b28fd028424432bb38e2f458c0a
Sonu-Shettiyar <sonushettiyar9393@gmail.com>
2025-08-25 13:50:32 +0530

    Added: Tooltip and taskprovider for context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TaskProvider } from "@/contexts/TaskContext";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <TaskProvider>
+          <Sonner />
+          <Component {...pageProps} />;
+        </TaskProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  )
 }
